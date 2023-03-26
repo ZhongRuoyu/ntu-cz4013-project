eb7d2eb670d91757f7dcd1f06fec105cca1fccae
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-27 04:43:35 +0800

    Add utility function for timestamp formatting

---
+++ src/utils/time.cc
@@ -0,0 +1,22 @@
+#include "utils/time.h"
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+namespace dfis {
+
+std::string FormatTimestamp(srpc::i64 timestamp, const char *format) {
+  std::time_t timepoint = std::chrono::system_clock::to_time_t(
+      std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>{
+          std::chrono::seconds(timestamp)});
+  tm local_time;
+  localtime_r(&timepoint, &local_time);
+  std::ostringstream ss;
+  ss << std::put_time(&local_time, format);
+  return ss.str();
+}
+
+}  // namespace dfis
---
+++ src/utils/time.h
@@ -0,0 +1,17 @@
+#ifndef DFIS_UTILS_TIME_H_
+#define DFIS_UTILS_TIME_H_
+
+#include <string>
+
+#include "srpc/types/integers.h"
+
+namespace dfis {
+
+inline constexpr const char *kISO8601Format = "%Y-%m-%dT%H:%M:%S%z";
+
+[[nodiscard]] std::string FormatTimestamp(srpc::i64 timestamp,
+                                          const char *format = kISO8601Format);
+
+}  // namespace dfis
+
+#endif  // DFIS_UTILS_TIME_H_
