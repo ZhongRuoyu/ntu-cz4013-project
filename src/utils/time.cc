7e4b7e1c97e9988afff858ed667b5dbf95cb777c
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-28 03:26:14 +0800

    Use `std::tm` instead of `tm`

--- src/utils/time.cc
+++ src/utils/time.cc
@@ -12,7 +12,7 @@ std::string FormatTimestamp(srpc::i64 timestamp, const char *format) {
   std::time_t timepoint = std::chrono::system_clock::to_time_t(
       std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>{
           std::chrono::seconds(timestamp)});
-  tm local_time;
+  std::tm local_time;
   localtime_r(&timepoint, &local_time);
   std::ostringstream ss;
   ss << std::put_time(&local_time, format);
